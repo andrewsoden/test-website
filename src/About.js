954362b2d8139dd4ef0fe56a4951fb62d8249038
Andrew Soden <andrewsoden@hotmail.com>
2020-06-07 17:05:38 +1000

    Add basic navigation

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Container from 'react-bootstrap/Container';
+
+class About extends Component {    
+    render() {
+
+    return(
+        <Container>
+            <h2>About</h2>
+        </Container>
+        );
+    }
+}
+
+export default About;
---
+++ src/Header.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+class Header extends Component {    
+    render() {
+
+    return(
+        <MemoryRouter>
+
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Navbar.Brand href="/">AS</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    <Nav.Link href="/about">About</Nav.Link>
+                    <Nav.Link href="/users">Users</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </MemoryRouter>
+        );
+    }
+}
+
+export default Header;
